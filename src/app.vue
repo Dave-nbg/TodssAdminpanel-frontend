38644eeede30b47f08f368a22fe8f66e7cdc6ff5
Dave-nbg <74248265+Dave-nbg@users.noreply.github.com>
2022-04-22 11:35:06 +0200

    setup made

---
+++ src/app.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+<p>TESTje</p>
+</template>
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import {createRouter, createWebHistory} from "vue-router";
+import './main.css'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+    ],
+})
+
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
