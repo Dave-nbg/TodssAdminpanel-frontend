b9f37736f1734300e9319492594736e0a278f7fd
Dave-nbg <74248265+Dave-nbg@users.noreply.github.com>
2022-04-26 11:09:33 +0200

    router

--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import LoginPage from './Views/inlog/loginpage';
+import UserPage from './Views/users/userpage';
+import CategoryPage from './Views/category/categorypage';
+import LocationPage from './Views/location/locationpage';
+import MenuItemPage from './Views/menuitem/menuitempage';
 import {createRouter, createWebHistory} from "vue-router";
 import './main.css'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/', component: App }
+        { path: '/', component: App },
+        { path: '/inlog', name: 'Inlog' component: LoginPage},
+        { path: '/gebruikers', component: UserPage},
+        { path: '/categorieën', component: CategoryPage},
+        { path: '/locaties', component: LocationPage},
+        { path: '/menuitems', component: MenuItemPage}
     ],
 })
 
