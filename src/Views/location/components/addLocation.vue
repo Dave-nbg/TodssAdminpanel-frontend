9bc34d00b74135a28586f8b2eeada1c1a6665e1d
Donny van Walsem <donny.vanwalsem@student.hu.nl>
2022-05-16 09:50:20 +0200

    WIP locatie pagina

--- src/Views/location/components/addLocation.vue
+++ src/Views/location/components/addLocation.vue
@@ -10,16 +10,6 @@ onMounted(async () => {
   locations.value = await locationService.getLocations();
 });
 
-function openLocationMenu(){
-  document.querySelector('#location').style.display = "block";
-  document.querySelector('#addButton').style.display = "none";
-}
-
-function closeScreen(){
-  document.querySelector('#location').style.display = "none";
-  document.querySelector('#addButton').style.display = "block";
-}
-
 async function addLocation() {
   let location = new Location(document.querySelector('#locationName').value)
   await locationService.postLocation(location);
@@ -39,22 +29,7 @@ async function editLocation(){
 
 <template>
 
-  <div id="addLocationMenu" class="w-full h-full">
-    <button id="addButton" class="bg-primary-500 text-white float-right h-10 p-2  rounded" @click="openLocationMenu()">Toevoegen</button>
 
-    <div id="location" class="hidden text-center bg-gray-300 border-2 rounded">
-
-        <button class="float-right bg-red-700 text-white w-9 h-9 content-center" @click="closeScreen()">X</button>
-        <strong class="mb-10">Locatie toevoegen</strong>
-        <form>
-          <div class="flex flex-col mb-10">
-            <label for="locationName">Locatie naam:</label>
-            <input id="locationName" name="locatieNaam" class="rounded border-2" placeholder="Locatie">
-          </div>
-          <button class="bg-primary-500 my-3 h-12 rounded p-3 w-max" type="button" @click="addLocation">locatie toevoegen</button>
-        </form>
-
-    </div>
 
 
     <div v-for="location in locations">
@@ -64,5 +39,4 @@ async function editLocation(){
       <input type="button" value="✏️" class="edit">
       <input type="button" value="🗑️" class="delete">
     </div>
-  </div>
 </template>
--- src/Views/location/locationpage.vue
+++ src/Views/location/locationpage.vue
@@ -1,19 +1,85 @@
 <script setup>
-import AddLocation from "./components/addLocation.vue";
 import TheSidebar from "../../components/TheSidebar.vue";
+import {computed, onMounted, ref} from 'vue'
+import { Dialog,DialogPanel,DialogTitle } from '@headlessui/vue'
+import {Location} from "./domain/location";
+import {LocationService} from "./service/locationService";
+
+const locationService = new LocationService();
+const isOpen = ref(false)
+const locations = ref([]);
+
+onMounted(async () => {
+    locations.value = await locationService.getLocations();
+});
+
+function setIsOpen(value) {
+    isOpen.value = value
+}
+
+async function addLocation() {
+    let location = new Location(document.querySelector('#locationName').value)
+
+    locationService.postLocation(location).then(response => {
+        locations.value.push(new Location(response.name));
+    });
+
+    setIsOpen(false);
+}
+
+async function editLocation(){
+    await locationService.editLocation(1,"test");
+    locations.value = await locationService.getLocations();
+}
+
+
 </script>
 
 <template>
 
-  <body class="grid grid-areas-layout grid-cols-layout grid-rows-layout h-full w-full">
-  <nav class="grid-in-nav">
-    <TheSidebar></TheSidebar>
-  </nav>
-  <main class="grid-in-main grid">
-    <h1 class="m-auto mt-5 font-bold text-2xl">Locatie</h1>
-    <div class="flex m-10">
-    <p>zoekbalk</p>
-    <add-location></add-location></div>
-  </main>
-  </body>
+    <div class="grid grid-areas-layout grid-cols-layout grid-rows-layout h-full w-full">
+        <nav class="grid-in-nav">
+            <TheSidebar></TheSidebar>
+        </nav>
+        <main class="grid-in-main grid">
+            <h1 class="m-auto mt-5 font-bold text-2xl">Locatie</h1>
+            <div class="flex m-10">
+                <p>zoekbalk</p>
+                <div id="addLocationMenu" class="w-full h-full">
+                    <button id="addButton" class="bg-primary-500 text-white float-right h-10 p-2  rounded" @click="setIsOpen(true)">Toevoegen</button>
+                </div>
+            </div>
+
+            <div v-for="(location, index) in locations" :key="index">
+                <label class="opmerking-item" for="opmerking">
+                    <span class="ml-10">{{location.name}}</span>
+                </label>
+                <input type="button" value="✏️" class="edit">
+                <input type="button" value="🗑️" class="delete">
+            </div>
+        </main>
+
+
+        <Dialog :open="isOpen" @close="setIsOpen" class="relative z-50">
+            <div class="fixed inset-0 bg-black/70" aria-hidden="true"></div>
+            <div class="fixed flex items-center inset-0 justify-center">
+                <DialogPanel class="bg-white rounded">
+                    <button class="float-right bg-red-700 text-white w-9 h-9 content-center rounded-tr" @click="setIsOpen(false)">X</button>
+                    <div class="p-8">
+                        <DialogTitle class="text-lg ">Locatie Aanmaken</DialogTitle>
+
+
+                        <form class="mt-8">
+                            <div class="flex flex-col mb-10">
+                                <label for="locationName">Locatie naam:</label>
+                                <input id="locationName" name="locatieNaam" class="rounded border-2 mt-1 p-2" placeholder="Locatie">
+                            </div>
+                        </form>
+                        <button @click="addLocation" class="bg-primary-500 my-2 h-12 rounded p-3">Locatie toevoegen</button>
+                    </div>
+                </DialogPanel>
+
+            </div>
+        </Dialog>
+    </div>
 </template>
