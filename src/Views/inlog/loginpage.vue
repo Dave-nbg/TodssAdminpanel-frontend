b47e86b6db00342ed9f962c5a21e4f56103fba22
Dave-nbg <74248265+Dave-nbg@users.noreply.github.com>
2022-06-13 11:31:56 +0200

    login with auth

--- src/Views/inlog/loginpage.vue
+++ src/Views/inlog/loginpage.vue
@@ -1,7 +1,26 @@
 <script setup>
-function loggedIn(){
-  location.href="/menuitems"
-}
+function  loggedIn(){
+    const URL = "http://localhost:8080/login";
+    const fetchOptions = {
+      method: "POST",
+      body: JSON.stringify({
+        username: document.querySelector("#email").value,
+        password: document.querySelector("#password").value
+      }),
+      headers: {
+        "content-type": "application/json",
+      },
+      credentials: "include"
+    };
+    fetch(URL, fetchOptions)
+        .then(response => response.ok
+            ? window.sessionStorage.setItem("myJWT", response.headers.get("authorization").split(" ").at(1))
+            : this._throwError(response.message))
+        .then(() => window.location.href = "/menuitems")
+        .catch(error => alert("Foute gebruikersnaam of wachtwoord."));
+  }
+
+
 </script>
 
 <template>
@@ -20,7 +39,6 @@ function loggedIn(){
       <label for="password">wachtwoord</label>
       <input type="password" name="password" id="password" class="border-2 mb-3 rounded">
       <button type="button" class="bg-primary-500 h-10 rounded text-white my-3" @click="loggedIn">Inloggen</button>
-      <a href="" class="underline text-primary-500 m-auto">Wachtwoord vergeten?</a>
     </div>
   </form>
   </div>
